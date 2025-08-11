492c1a5b9fb43c972ccfaadace705ec80d326eb4
JuanCastillo29 <Juan.castillo.sanz@gmail.com>
2025-08-11 18:12:23 +0200

    Implement safer volatility calc

--- scripts/Heston_MonteCarlo_simulation.c
+++ scripts/Heston_MonteCarlo_simulation.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 double normal_std() {
     double u1 = rand() / (RAND_MAX + 1.0);
     double u2 = rand() / (RAND_MAX + 1.0);
+    if (u1 < 1e-10)
+    u1 = 1e-10;
     return sqrt(-2.0 * log(u1)) * cos(2 * M_PI * u2);
 }
 
 __declspec(dllexport)
 void Heston(double S0, double v0, double mu, double kappa, double theta, double sigma, double rho, double T, double *vol, double *precios, int N, int seed) {
-	srand(seed);
+    srand(seed);
     double dt = T / N;
     double S = S0;
-    double v = v0*v0;
+    double v = v0 * v0;
 
     precios[0] = S;
     vol[0] = v0;
@@ -24,12 +27,14 @@ void Heston(double S0, double v0, double mu, double kappa, double theta, double 
         double w1 = z1;
         double w2 = rho * z1 + sqrt(1 - rho * rho) * z2;
 
-        if (v < 0) v = 0;
+        double sqrt_v = sqrt(fmax(v, 0.0));
 
-        S = S + mu * S * dt + sqrt(v * dt) * S * w1;
-        v = v + kappa * (theta - v) * dt + sigma * sqrt(v * dt) * w2;
+        S = S * exp((mu - 0.5 * v) * dt + sqrt_v * sqrt(dt) * w1);
+        v = v + kappa * (theta - v) * dt + sigma * sqrt_v * sqrt(dt) * w2;
+        v = fmax(v, 0.0);
 
         precios[i] = S;
         vol[i] = sqrt(v);
     }
 }
+
